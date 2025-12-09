2d5070ec1ba386a2395691234083763c8096f6a7
amirulabdlatib <amirulal73@gmail.com>
2025-12-09 16:48:24 +0800

    Update seeder sequence and assign user to the role

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,25 +15,34 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        $admin = User::create([
             'name' => 'Admin',
             'email' => "admin@crm.com",
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
         ]);
 
-        User::create([
+        $manager = User::create([
             'name' => 'Sales Manager',
             'email' => "salesmanager@crm.com",
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
         ]);
 
-        User::create([
+        $salesperson = User::create([
             'name' => 'Sales Person',
             'email' => "salesperson@crm.com",
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
         ]);
+
+        $adminRole = Role::where('name', 'Admin')->pluck('id');
+        $admin->roles()->sync($adminRole);
+
+        $managerRole = Role::where('name', 'Manager')->pluck('id');
+        $manager->roles()->sync($managerRole);
+
+        $salespersonRole = Role::where('name', 'Sales Person')->pluck('id');
+        $salesperson->roles()->sync($salespersonRole);
     }
 }
